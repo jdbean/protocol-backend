fb4818f7a634046e8b71e73c5c1e18fe4066cc7a
github username <jeremyagottfried@gmail.com>
2018-05-08 09:54:57 -0400

    websocket with auth

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,54 +1,54 @@
 require 'byebug'
-# module ApplicationCable
-#   class Connection < ActionCable::Connection::Base
-#     identified_by :current_user
-#
-#     def connect
-#       byebug
-#       self.current_user = find_verified_user
-#     end
-#
-#     private
-#       def find_verified_user
-#         byebug
-#         if verified_user = User.find_by(name: "Jeremy")
-#           verified_user
-#         else
-#           reject_unauthorized_connection
-#         end
-#       end
-#   end
-# end
-
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
-    # rescue_from ActiveRecord::RecordNotFound, JWT::DecodeError, CustomErrorClass do
-    #   reject_unauthorized_connection
-    # end
-
     def connect
-      @jwt_token = request.params[:token]
-      # || raise(CustomErrorClass)
       self.current_user = find_verified_user
     end
-
+# FIXME: AUTHORIZATION 
     private
-
-    attr_reader :jwt_token
-
-    def find_verified_user
-      payload, = decode_token
-      User.find(payload['sub'])
-    end
-
-    def decoding_key
-      Knock.token_secret_signature_key.call
-    end
-
-    def decode_token
-      JWT.decode(jwt_token.to_s, decoding_key)
-    end
+      def find_verified_user
+       byebug
+        if verified_user = User.find_by(name: "Jeremy")
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
+
+# module ApplicationCable
+#   class Connection < ActionCable::Connection::Base
+#     identified_by :current_user
+#
+#     # rescue_from ActiveRecord::RecordNotFound, JWT::DecodeError, CustomErrorClass do
+#     #   reject_unauthorized_connection
+#     # end
+#
+#     def connect
+#       @jwt_token = request.params[:token]
+#       # || raise(CustomErrorClass)
+#       self.current_user = find_verified_user
+#     end
+#
+#     private
+#
+#     attr_reader :jwt_token
+#
+#     def find_verified_user
+#       byebug
+#       payload, = decode_token
+#       User.find(payload['sub'])
+#     end
+#
+#     def decoding_key
+#       Knock.token_secret_signature_key.call
+#     end
+#
+#     def decode_token
+#       JWT.decode(jwt_token.to_s, decoding_key)
+#     end
+#   end
+# end
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,5 +1,9 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
+    byebug
     stream_from "my_channel_#{current_user.id}"
   end
+
+
+
 end

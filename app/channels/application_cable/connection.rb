596bd335223a2a9e1f6d617ecb59806cda2aa6b0
github username <jeremyagottfried@gmail.com>
2018-05-08 12:18:55 -0400

    working auth with web sockets

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,54 +1,53 @@
 require 'byebug'
-# module ApplicationCable
-#   class Connection < ActionCable::Connection::Base
-#     identified_by :current_user
-#
-#     def connect
-#       byebug
-#       self.current_user = find_verified_user
-#     end
-#
-#     private
-#       def find_verified_user
-#         byebug
-#         if verified_user = User.find_by(name: "Jeremy")
-#           verified_user
-#         else
-#           reject_unauthorized_connection
-#         end
-#       end
-#   end
-# end
-
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
-    #
-    # # rescue_from ActiveRecord::RecordNotFound, JWT::DecodeError, CustomErrorClass do
-    # #   reject_unauthorized_connection
-    # # end
-    #
-    # def connect
-    #   @jwt_token = request.params[:token]
-    #   # || raise(CustomErrorClass)
-    #   self.current_user = find_verified_user
-    # end
-    #
-    # private
-    #
-    # attr_reader :jwt_token
-    #
-    # def find_verified_user
-    #   payload, = decode_token
-    #   User.find(payload['sub'])
-    # end
-    #
-    # def decoding_key
-    #   Knock.token_secret_signature_key.call
-    # end
-    #
-    # def decode_token
-    #   JWT.decode(jwt_token.to_s, decoding_key)
-    # end
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+
+        if verified_user = User.find(cookies[:user_id])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
+
+# module ApplicationCable
+#   class Connection < ActionCable::Connection::Base
+#     # identified_by :current_user
+#     #
+#     # # rescue_from ActiveRecord::RecordNotFound, JWT::DecodeError, CustomErrorClass do
+#     # #   reject_unauthorized_connection
+#     # # end
+#     #
+#     # def connect
+#     #   @jwt_token = request.params[:token]
+#     #   # || raise(CustomErrorClass)
+#     #   self.current_user = find_verified_user
+#     # end
+#     #
+#     # private
+#     #
+#     # attr_reader :jwt_token
+#     #
+#     # def find_verified_user
+#     #   payload, = decode_token
+#     #   User.find(payload['sub'])
+#     # end
+#     #
+#     # def decoding_key
+#     #   Knock.token_secret_signature_key.call
+#     # end
+#     #
+#     # def decode_token
+#     #   JWT.decode(jwt_token.to_s, decoding_key)
+#     # end
+#   end
+# end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -43,7 +43,7 @@ module Api
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_user
-          @user = User.find(params[:id])
+          @user = User.find_by(name: params[:id])
         end
 
         # Only allow a trusted parameter "white list" through.
